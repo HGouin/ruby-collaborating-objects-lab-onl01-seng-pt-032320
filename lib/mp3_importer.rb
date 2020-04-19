829fafea736ea706a6bb1cdcc37f3bdb8464b36a
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-19 19:56:35 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,7 +4,7 @@ class MP3Importer
 
   def initialize(file_path)
     @path = file_path
-    @files = Dir.entries(@path).filter{|f| File.file? f }.collect{ |file| file.split("/").last }
+    @files = Dir.entries(@path + "/*").collect{ |file| file.split("/").last }
 
   end
 
